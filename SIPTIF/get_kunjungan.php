66a980f944c99cd6b891066c300b12d876a4260e
Masamune-Kenzaki <masamunekenzaki@gmail.com>
2025-11-23 12:27:05 +0700

    menambahkan koneksi database

---
+++ SIPTIF/get_kunjungan.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: application/json');
+include 'db_config.php';
+
+$sql = "SELECT * FROM kunjungan ORDER BY tanggal DESC, masuk DESC";
+$result = mysqli_query($conn, $sql);
+
+$data = [];
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
+mysqli_close($conn);
+?>
